ab5bdb03ef86e476abade8ad06094bb2428ea848
JOCKER3201 <michaelfurtak@outlook.com>
2026-04-29 18:37:32 +0200

    little fixes

--- extension.js
+++ extension.js
@@ -14,11 +14,20 @@ export default class DashToDockExtension extends Extension.Extension {
         this._settings = this.getSettings(
             'org.gnome.shell.extensions.super-desktop');
         this._activitiesButton = Main.panel.statusArea.activities;
+        this._dateMenu = Main.panel.statusArea.dateMenu;
+        this._dateMenuOriginalParent = this._dateMenu?.container?.get_parent() ?? null;
+        this._dateMenuOriginalIndex = this._dateMenuOriginalParent
+            ? this._dateMenuOriginalParent.get_children().indexOf(this._dateMenu.container)
+            : -1;
 
         this._applyActivitiesVisibility();
+        this._applyDateMenuPosition();
         this._activitiesChangedId = this._settings.connect(
             'changed::hide-activities-button',
-            () => this._applyActivitiesVisibility());
+            () => {
+                this._applyActivitiesVisibility();
+                this._applyDateMenuPosition();
+            });
 
         if (Main.layoutManager.removeHotCorner)
             Main.layoutManager.removeHotCorner();
@@ -37,6 +46,11 @@ export default class DashToDockExtension extends Extension.Extension {
         }
         this._activitiesButton = null;
 
+        this._restoreDateMenuPosition();
+        this._dateMenu = null;
+        this._dateMenuOriginalParent = null;
+        this._dateMenuOriginalIndex = -1;
+
         if (Main.layoutManager.addHotCorner)
             Main.layoutManager._updateHotCorners();
 
@@ -54,5 +68,34 @@ export default class DashToDockExtension extends Extension.Extension {
             this._activitiesButton.show();
             this._activitiesButton.container?.show();
         }
+    }
+
+    _applyDateMenuPosition() {
+        const container = this._dateMenu?.container;
+        if (!container)
+            return;
+        const parent = container.get_parent();
+        if (this._settings.get_boolean('hide-activities-button')) {
+            if (parent !== Main.panel._leftBox) {
+                parent?.remove_child(container);
+                Main.panel._leftBox.insert_child_at_index(container, -1);
+            }
+        } else {
+            this._restoreDateMenuPosition();
+        }
+    }
+
+    _restoreDateMenuPosition() {
+        const container = this._dateMenu?.container;
+        if (!container || !this._dateMenuOriginalParent)
+            return;
+        const parent = container.get_parent();
+        if (parent === this._dateMenuOriginalParent)
+            return;
+        parent?.remove_child(container);
+        const index = Math.min(this._dateMenuOriginalIndex,
+            this._dateMenuOriginalParent.get_children().length);
+        this._dateMenuOriginalParent.insert_child_at_index(container,
+            index < 0 ? -1 : index);
     }
 }
